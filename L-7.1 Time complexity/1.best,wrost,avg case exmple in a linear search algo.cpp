5cedd7d8f9d9aabec153896562ce6fb6262a5329
shakilahamedriaz <shakilahamedriaz@gmail.com>
2023-01-07 03:22:45 +0600

    done

---
+++ L-7.1 Time complexity/1.best,wrost,avg case exmple in a linear search algo.cpp
@@ -0,0 +1,30 @@
+/*
+
+suppose some num: 20 10 5 100 300 15 1222
+
+
+x= 238;
+
+x=20;
+
+ find these num and explain the cases :
+
+
+
+   ** for finding 238 we have to traverse all these case elements / thats why need more time
+        more run time  etc..
+        this called Wrost case : for liner search it explain : ∝(proposional sing) n
+
+
+    ** Best case is 20 cZ 1st elements need little runtimne etc;
+
+
+         Best case explain for liner  : constest
+
+
+
+     **Avg case : (n(n-1)/2)/2;
+                or  n+1/2
+
+
+

0c5d0cdaf885db1cf8347a3a0f703f0e4e39e6bb
shakilahamedriaz <shakilahamedriaz@gmail.com>
2023-01-12 22:27:23 +0600

    doen

--- L-9.2 Two Dimensional Array Qs/3.matrix search.cpp
+++ L-9.2 Two Dimensional Array Qs/3.matrix search.cpp
@@ -1,1 +1,46 @@
+#include<bits/stdc++.h>
+using namespace std;
 
+int main(){
+
+int  n,m; cin>>n>>m;
+int target; cin>>target;
+int mat[n][m];
+
+for(int i=0; i<n; i++){
+
+    for(int j=0; j<m; j++){
+
+        cin>>mat[i][j];
+    }
+}
+
+int r=0, c= m-1;
+bool found=false;
+while(r<n && c>=0){
+
+    if(mat[r][c]==target){
+
+        found=true;
+    }
+
+    if(mat[r][c]> target){
+
+        c--;
+    }else{
+
+      r++;
+
+    }
+}
+
+if(found){
+
+    cout<< "Element found"<<endl;
+}else{
+
+  cout<< "Element Not found"<<endl;
+}
+
+return 0;
+}

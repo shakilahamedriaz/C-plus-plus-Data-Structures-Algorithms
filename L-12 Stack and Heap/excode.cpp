d85188cac69f3f2e7beda5cf8d823d60dc49989a
shakilahamedriaz <shakilahamedriaz@gmail.com>
2023-01-13 01:17:43 +0600

    done

---
+++ L-12 Stack and Heap/excode.cpp
@@ -0,0 +1,29 @@
+//for details go yt : https://www.youtube.com/watch?v=bbym08gSWvQ&list=PLfqMhTWNBTe0b2nM6JHVCnAkhQRGiZMSJ&index=30
+// and : https://www.youtube.com/watch?v=xV4S2PCuLr0
+
+
+
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+
+int a=10;  //part of stack
+int b=40;
+
+
+
+int sum=a+b;
+ //part of stack these data and fromn there data are go to
+ // processor and process to add values a + b
+ //then print and output will show as screen
+
+ //Hardrix->ram ->display
+
+
+
+cout<<sum<<endl;
+
+return 0;
+}

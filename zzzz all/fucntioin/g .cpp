acc9431ada6cfacee2fbf690cbeb4de70a9dfe6d
shakilahamedriaz <shakilahamedriaz@gmail.com>
2023-03-01 01:53:24 +0600

    ok

---
+++ zzzz all/fucntioin/g .cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void bal(int a,int b){
+
+int sum=a+b;
+
+cout<<sum<<endl;
+}
+
+int main()
+{
+    int a,b;
+    cin>>a>>b;
+
+    bal(a,b);
+
+    return 0;
+}
